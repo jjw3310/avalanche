9eebae8de68e8076c1a989e54a9c75664bc002c9
yyyljy <yyyljy@gmail.com>
2023-05-15 21:32:36 +0900

    C:/Program Files/Git/contract/userDate.sol created

--- contract/userData.sol
+++ contract/userData.sol
@@ -9,56 +9,67 @@ contract UserData {
         bytes32 password;
         uint256 createdAt;
         uint256 updatedAt;
-        uint256 loginAt; // gas may required when user login
+        //uint256 loginAt; // gas may required when user login
     }
 
     mapping(string => User) userList; // id => User
     mapping(address => string) userID; // wallet addr => user id
 
 //Max ERR NUM = 3
+    //signUp INPUT: id,pass     RETURN:bool
     function signUp(string memory _id, string memory _password) public returns (bool)
-    {
+    {//id,pass input value check && id check
         require(bytes(_id).length * bytes(_password).length > 0, "_ERR[3]:Check Your Input Values");
         require(userList[_id].createdAt == 0, "_ERR[0]:ID Already Exists");
 
         bytes32 pass = keccak256(abi.encodePacked(_password));
-        userList[_id] = User(address(0), pass, block.timestamp, 0, 0);
+        userList[_id] = User(address(0), pass, block.timestamp, 0);
         return true;
     }
 
-    function login(string memory _id, string memory _password) public returns (bool) {
+    //login with id & password
+    function login(string memory _id, string memory _password) public view returns (bool)
+    {//id check && password check
         require(userList[_id].createdAt > 0, "_ERR[1]:Check Your ID or Password");
         require(userList[_id].password == keccak256(abi.encodePacked(_password)), "_ERR[1]:Check Your ID or Password");
-        // return true; // no gas required
+        return true; // no gas required
 
-        userList[_id].loginAt = block.timestamp;
-        return true;
+        // userList[_id].loginAt = block.timestamp;
+        // return true;
     }
 
-    function getUser(string memory _id) public view returns (User memory) {
+    //return user data without password
+    function getUser(string memory _id) public view returns (User memory)
+    {//check id
         require(userList[_id].createdAt > 0, "_ERR[2]:ID Doesn't Exist");
         User memory tmp = userList[_id];
         tmp.password = 0;
         return tmp;
     }
 
+    //delete user data
     function deleteUser(string memory _id, string memory _password) public returns (bool) {
         require(userList[_id].createdAt > 0, "_ERR[1]:Check Your ID or Password");
         require(userList[_id].password == keccak256(abi.encodePacked(_password)), "_ERR[1]:Check Your ID or Password");
         
+        delete userID[userList[_id].addr];
         delete userList[_id];
         return true;
     }
 
+    //update user wallet address
     function updateUserAddress(string memory _id, string memory _password, address _addr) public returns (bool) {
         require(userList[_id].createdAt > 0, "_ERR[1]:Check Your ID or Password");
         require(userList[_id].password == keccak256(abi.encodePacked(_password)), "_ERR[1]:Check Your ID or Password");
         
         userList[_id].addr = _addr;
         userList[_id].updatedAt = block.timestamp;
+
+        userID[_addr] = _id;
         return true;
     }
 
+    //change user password when knows present password
     function updateUserPassword(string memory _id, string memory _password, string memory _nextPassword) public returns (bool) {
         require(bytes(_id).length * bytes(_password).length > 0, "_ERR[3]:Check Your Input Values");
         require(userList[_id].createdAt > 0, "_ERR[1]:Check Your ID or Password");
