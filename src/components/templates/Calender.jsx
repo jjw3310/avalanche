acef6a9c48f44f62546f1099445f1584d5ca825b
Jeongho Park <d0lbangi13@naver.com>
2023-05-18 01:25:03 +0900

    feat: Commit the web codes before Merge

--- src/components/templates/Calender.jsx
+++ src/components/templates/Calender.jsx
@@ -39,12 +39,141 @@ const Calender = forwardRef((props, ref) => {
           pt={5}
           pb={5}
         >
-          <Box borderRadius={"30px"} h="100%" bg="gray.500" mr={"20px"} flex="2">
+          {/* 큰 캘린더 */}
+          {/* <Box borderRadius={"30px"} h="100%" bg="gray.500" mr={"20px"} flex="2"></Box> */}
+
+          {/* 작은 캘린더 민팅 이후 */}
+          <Box borderRadius={"30px"} h="100%" bg="gray.600" ml={"20px"} flex="1">
+            <Box
+              
+            >
+              {/* #upper box */}
+              <Flex direction={"column"} justify={"flex-start"} align={"flex-start"} w={"100%"} h="200px" >
+                {/* #1 */}
+                <Flex>
+                  
+                </Flex>
+                <Flex
+                  ml={"20px"}
+                  mt={"10px"} 
+                  fontFamily="Raleway" 
+                  fontStyle="normal" 
+                  fontWeight={700} 
+                  fontSize={["20px", null, "40px"]} 
+                  lineHeight={["23px", null, "47px"]} 
+                  color="#FFFFFF"
+                >
+                  My birthday
+                </Flex>
+                <Flex 
+                  ml={"20px"}
+                  mt={"70px"} 
+                  fontFamily="Raleway" 
+                  fontStyle="normal" 
+                  fontWeight={400} 
+                  fontSize={["20px", null, "24px"]} 
+                  lineHeight={["23px", null, "28px"]} 
+                  color="#FFFFFF"
+                >
+                  Date
+                </Flex>
+                {/* #2 */}
+                <Flex>
+
+                </Flex>
+                {/* #3 */}
+                <Flex 
+                  position={"relative"}
+                >
+                  <Image 
+                    src={DescriptionStarLight2} 
+                    position={"absolute"}
+                    left={"-20px"}
+                    bottom={"-30px"}
+                    width={"100px"}
+                    height={"100px"}
+                  />
+                  <Text
+                    ml={"50px"}
+                    mb={"2px"}
+                    fontFamily={"Raleway"}
+                    fontStyle={"normal"}
+                    fontWeight={"700"}
+                    fontSize={"20px"}
+                    lineHeight={"38px"}
+                    color={"#FFFFFF"}
+                    // position={"relative"}
+                  >
+                  20230522
+                  </Text>
+                </Flex>
+
+              </Flex>
+            </Box>
+            
+            {/* #below box */}
+            <Box 
+              background="linear-gradient(180deg, rgba(52, 71, 88, 0.9) 0%, rgba(56, 89, 120, 0) 100%)"
+              boxSizing="border-box"
+              border="1px solid #ADADAD"
+              borderTopLeftRadius="0px"
+              borderTopRightRadius="0px"
+              borderBottomRightRadius="30px"
+              borderBottomLeftRadius="30px"
+              w={"100%"} 
+              h={"360px"}
+            >
+
+              <Flex direction={"column"} justify={"flex-start"} align={"flex-start"}>
+                  
+                <Box
+                  mt={"10px"}
+                  ml={"20px"}
+                  fontFamily={"Raleway"}
+                  fontStyle={"normal"}
+                  fontWeight={"700"}
+                  fontSize={"16px"}
+                  lineHeight={"23px"}
+                  color={"#FFFFFF"}
+                  justify={"center"} 
+                  align={"center"}
+                >
+                Date
+                </Box>
+
+
+                
+
+                <Button mt={"5px"} ml={"120px"} w={"120px"} h={"30px"} borderRadius={"20px"} _hover={{ bg: "purple.500" }} >
+                <Popover trigger={"hover"} placement={"bottom-start"}>
+                  <PopoverTrigger>
+                    <Box
+                      fontWeight={"500"}
+                      color="black"
+                      fontStyle={"normal"}
+                      fontSize={{ base: "10px", md: "15px" }}
+                      lineHeight={"30px"}
+                      justifyContent={"flex-end"}
+                      alignItems={"flex-end"}
+                      _hover={{
+                        textDecoration: "none",
+                        color: "linkHoverColor",
+                      }}
+                      href="/"
+                      target="_blank"
+                    >
+                        Mint the date
+                    </Box>
+                  </PopoverTrigger>
+                </Popover>
+              </Button>
+              </Flex>
+            </Box>
+            
           </Box>
 
 
-
-        
+          {/* 작은 캘린더 민팅 이전 */}
           <Box direction={"column"} justify={"center"} align={"center"} borderRadius={"30px"} h="100%" bg="white" ml={"20px"} flex="1">
             <Box>
               {/* #1 top box */}
