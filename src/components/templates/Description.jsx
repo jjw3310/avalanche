ea7ab5710f4c80fa87c1b32778829d74647f793f
Jeongho Park <d0lbangi13@naver.com>
2023-05-17 16:34:33 +0900

    feat: updated DescriptionPage with logos

--- src/components/templates/Description.jsx
+++ src/components/templates/Description.jsx
@@ -7,14 +7,15 @@ import {
   Text,
   SimpleGrid,
   Image,
-  ListItem,
   UnorderedList,
 } from "@chakra-ui/react";
 // import mento1 from "@assets/images/mento1.png";
 // import mento2 from "@assets/images/mento2.png";
 // import mento3 from "@assets/images/mento3.png";
 import DescriptionStarLight2 from "@assets/images/DescriptionStarLight2.png";
-
+import logoCalendar from "@assets/images/logoCalendar.png";
+import palateIcon from "@assets/images/palateIcon.png";
+import logoPeople from "@assets/images/logoPeople.png";
 const Description = forwardRef((props, ref) => {
   return (
     <Box 
@@ -38,21 +39,80 @@ const Description = forwardRef((props, ref) => {
           lineHeight={"82px"}
           font-family="Raleway"
           color={"#FFFFFF"}
-
+          mb={"100px"}
         >
           What You Can Do with Date’s
         </chakra.h1>
+
         <SimpleGrid
           columns={{ base: 1, md: 3 }}
           spacing={{ base: 20, md: 20, lg: 10 }}
         >
 
+          <Flex direction={"column"} justify={"center"} align={"center"}>
+            <Image 
+              src={logoCalendar}
+              // position="relative"
+              width="50px"
+              height="50px"
+              left="100px"
+              bottom="-80px"
+            />
+              <Text
+                mt={"100px"}
+                ml={"70px"}
+                fontFamily={"Raleway"}
+                fontStyle={"normal"}
+                fontWeight={"700"}
+                fontSize={"30px"}
+                lineHeight={"38px"}
+                color={"#FFFFFF"}
+                position={"relative"}
+              >
+              <Image 
+                src={DescriptionStarLight2} 
+                position={"absolute"}
+                width={"100px"}
+                height={"100px"}
+                left={"-70px"}
+                bottom={"-30px"}
+              />
+              Connect and celebrate
+            </Text>
+            <UnorderedList
+              mt={"30px"}
+              fontFamily={"Raleway"}
+              fontStyle={"normal"}
+              fontWeight={"700"}
+              fontSize={"16px"}
+              lineHeight={"23px"}
+              color={"#FFFFFF"}
+            >
+              <Text>
+              Join our social platform to connect, <br/>
+              share experiences, and celebrate together. <br/>
+              See other people's special dates, <br/>
+              leave comments, and mingle with others. <br/>
+              </Text>
+            </UnorderedList>
+          </Flex>
 
-          <Flex 
-            direction={"column"} 
-            justify={"center"} 
-            align={"center"}
-          >
+
+
+
+
+
+
+
+          <Flex direction={"column"} justify={"center"} align={"center"}>
+            <Image  
+              src={palateIcon}
+              // position="relative"
+              width="50px"
+              height="50px"
+              left="100px"
+              bottom="-80px"
+            />
             <Text
               mt={"100px"}
               ml={"40px"}
@@ -92,12 +152,15 @@ const Description = forwardRef((props, ref) => {
             </UnorderedList>
           </Flex>
 
-
-
-
-
-
           <Flex direction={"column"} justify={"center"} align={"center"}>
+            <Image  
+              src={logoPeople}
+              // position="relative"
+              width="50px"
+              height="50px"
+              left="100px"
+              bottom="-80px"
+            />
             <Text
               mt={"100px"}
               ml={"70px"}
@@ -136,54 +199,6 @@ const Description = forwardRef((props, ref) => {
               </Text>
             </UnorderedList>
           </Flex>
-
-
-
-
-
-          
-          <Flex direction={"column"} justify={"center"} align={"center"}>
-            <Text
-              mt={"100px"}
-              ml={"70px"}
-              fontFamily={"Raleway"}
-              fontStyle={"normal"}
-              fontWeight={"700"}
-              fontSize={"30px"}
-              lineHeight={"38px"}
-              color={"#FFFFFF"}
-              position={"relative"}
-            >
-              <Image 
-                src={DescriptionStarLight2} 
-                position={"absolute"}
-                width={"100px"}
-                height={"100px"}
-                left={"-70px"}
-                bottom={"-30px"}
-              />
-              Connect and celebrate
-            </Text>
-            <UnorderedList
-              mt={"30px"}
-              fontFamily={"Raleway"}
-              fontStyle={"normal"}
-              fontWeight={"700"}
-              fontSize={"16px"}
-              lineHeight={"23px"}
-              color={"#FFFFFF"}
-            >
-              <Text>
-              Join our social platform to connect, <br/>
-              share experiences, and celebrate together. <br/>
-              See other people's special dates, <br/>
-              leave comments, and mingle with others. <br/>
-              </Text>
-            </UnorderedList>
-          </Flex>
-
-
-
 
         </SimpleGrid>
       </Flex>
