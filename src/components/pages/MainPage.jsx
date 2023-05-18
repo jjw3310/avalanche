199a5765398af0db95d45799652546cfb07cd936
likejjw <jjw3310@gmail.com>
2023-05-19 00:44:18 +0900

    mumbai testnet

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -13,11 +13,21 @@ import { Button } from "@chakra-ui/react";
 // import { useGasless } from "@hooks/useGasless";
 
 import Web3 from "web3";
-import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../../web3.config";
+
+import {
+  // CONTRACT_ABI,
+  // CONTRACT_ADDRESS,
+  userContABI,
+  dateContABI,
+  commentContABI,
+  USER_CONTRACT_ADDRESS,
+  DATE_CONTRACT_ADDRESS,
+  COMMENT_CONTRACT_ADDRESS,
+} from "../../web3.config.js";
 // import { useGasless } from "@hooks/useGasless";
 
 const web3 = new Web3(window.ethereum);
-const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+// const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
 export default function MainPage() {
   const [totalNft, setTotalNft] = useState(0);
@@ -32,50 +42,50 @@ export default function MainPage() {
   const [account, setAccount] = useState();
   const [inputAcnt, setInputAcnt] = useState("test");
   const [nftInfo, setNftInfo] = useState();
-  const getTotalNft = async () => {
-    try {
-      if (!contract) return;
+  // const getTotalNft = async () => {
+  //   try {
+  //     if (!contract) return;
 
-      const response = await contract.methods.totalNft().call();
+  //     const response = await contract.methods.totalNft().call();
 
-      setTotalNft(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const getMintedNft = async () => {
-    try {
-      if (!contract) return;
+  //     setTotalNft(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // const getMintedNft = async () => {
+  //   try {
+  //     if (!contract) return;
 
-      const response = await contract.methods.totalSupply().call();
+  //     const response = await contract.methods.totalSupply().call();
 
-      setMintedNft(response);
-      //   console.log(response);
-      //   setPage(parseInt((parseInt(response) - 1) / 10) + 1);
-      setPage(Math.floor(parseInt(response - 1) / 3) + 1);
-      //   console.log(page);
-      // 10 - 1 = 9 / 10 = 0 + 1= 1page
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const getMyNft = async () => {
-    try {
-      if (!contract || !account) return;
-      const response = await contract.methods.balanceOf(account).call();
-      console.log("내꺼 : " + response);
-      setMyNft(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    getTotalNft();
-    getMintedNft();
-  }, []);
-  useEffect(() => {
-    getMyNft();
-  }, [account]);
+  //     setMintedNft(response);
+  //     //   console.log(response);
+  //     //   setPage(parseInt((parseInt(response) - 1) / 10) + 1);
+  //     setPage(Math.floor(parseInt(response - 1) / 3) + 1);
+  //     //   console.log(page);
+  //     // 10 - 1 = 9 / 10 = 0 + 1= 1page
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // const getMyNft = async () => {
+  //   try {
+  //     if (!contract || !account) return;
+  //     const response = await contract.methods.balanceOf(account).call();
+  //     console.log("내꺼 : " + response);
+  //     setMyNft(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   getTotalNft();
+  //   getMintedNft();
+  // }, []);
+  // useEffect(() => {
+  //   getMyNft();
+  // }, [account]);
 
   //   useGasless();
   var date = new Date();
