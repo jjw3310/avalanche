94e730675ef9a6c5455ad4cbd2b212be0ad8942b
likejjw <jjw3310@gmail.com>
2023-05-18 12:35:29 +0900

    set calander nft

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -7,10 +7,65 @@ import Description from "@components/templates/Description";
 import Faq from "@components/templates/Faq";
 import Footer from "@components/templates/Footer";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../../web3.config";
 // import { useGasless } from "@hooks/useGasless";
 
-export default function MainPage() {
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+export default function MainPage({ account }) {
+  const [totalNft, setTotalNft] = useState(0);
+  const [mintedNft, setMintedNft] = useState(0);
+  const [myNft, setMyNft] = useState(0);
+  const [page, setPage] = useState(1);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const getTotalNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalNft().call();
+
+      setTotalNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMintedNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalSupply().call();
+
+      setMintedNft(response);
+      //   console.log(response);
+      //   setPage(parseInt((parseInt(response) - 1) / 10) + 1);
+      setPage(Math.floor(parseInt(response - 1) / 3) + 1);
+      //   console.log(page);
+      // 10 - 1 = 9 / 10 = 0 + 1= 1page
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMyNft = async () => {
+    try {
+      if (!contract || !account) return;
+      const response = await contract.methods.balanceOf(account).call();
+      console.log("내꺼 : " + response);
+      setMyNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getTotalNft();
+    getMintedNft();
+  }, []);
+  useEffect(() => {
+    getMyNft();
+  }, [account]);
   //   useGasless();
   return (
     <>
@@ -19,7 +74,14 @@ export default function MainPage() {
       // onClickNavLink={handleClickNavLink}
       />
       <Introduce />
-      <Calender />
+      <Calender
+        selected={selectedDate}
+        onSelectDate={setSelectedDate}
+        totalNft={totalNft}
+        mintedNft={mintedNft}
+        myNft={myNft}
+        page={page}
+      />
       <Description />
       <Faq />
       <Footer />
--- src/components/templates/Calender.jsx
+++ src/components/templates/Calender.jsx
@@ -21,12 +21,25 @@ import { useState, useEffect } from "react";
 import previousMonth from "@assets/images/previousMonth.svg";
 import nextMonth from "@assets/images/nextMonth.svg";
 import DescriptionStarLight2 from "@assets/images/DescriptionStarLight2.png";
-
-const Calender = forwardRef((props, ref) => {
+import { Link } from "react-router-dom";
+import axios from "axios";
+const Calender = ({
+  selected,
+  onSelectDate,
+  totalNft,
+  mintedNft,
+  myNft,
+  page,
+}) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [images, setImages] = useState(["ss", "sa"]);
   const [formatDate, setFormatDate] = useState(["aa", "bb"]);
-  const [nfts, setNfts] = useState(["", "h"]);
+
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [nfts, setNfts] = useState();
+
+  // const [nfts, setNfts] = useState(["", "h"]);
+
   useEffect(() => {
     const fetchImages = async () => {
       const response = await fetch(
@@ -67,23 +80,102 @@ const Calender = forwardRef((props, ref) => {
   const walletAddress = "0x0c4E3dAc7B9595329911C21331E311E0E0Bf3ad5";
 
   // 지갑의 NFT 목록 가져오기
-  const getNfts = async () => {
+  // const getNfts = async () => {
+  //   try {
+  //     // const nfts = await web3.eth.getNFTs(walletAddress);
+  //     // setNfts(nfts);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getNfts();
+  // }, []);
+  const getNfts = async (p) => {
     try {
-      const nfts = await web3.eth.getNFTs(walletAddress);
-      setNfts(nfts);
+      let nftArray = [];
+
+      //한 번 비워주고 시작
+      setNfts();
+      for (let i = 0; i < 3; i++) {
+        //   for (let i = 0; i < 10; i++) {
+        const tokenId = i + 1 + (p - 1) * 3;
+        // console.log(tokenId);
+        let response = await axios.get(
+          `${process.env.REACT_APP_JSON_URL}/${tokenId}.json`
+        );
+        console.log("tooooo" + tokenId);
+        nftArray.push({ tokenId, metadata: response.data });
+        console.log(process.env.REACT_APP_JSON_URL);
+      }
+      setNfts(nftArray);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const onClickPageChange = (p) => () => {
+    setSelectedPage(p);
+    getNfts(p);
+  };
+
+  const pageComp = () => {
+    let pageArray = [];
+
+    for (let i = 0; i < page; i++) {
+      pageArray.push(
+        <button
+          key={i}
+          className={`ml-4 text-2xl font-bold hover:text-white ${
+            i + 1 === selectedPage ? "text-white" : "text-gray-400"
+          }`}
+          onClick={onClickPageChange(i + 1)}
+        >
+          {i + 1} <span className="text-base">페이지</span>
+        </button>
+      );
+    }
+    return pageArray;
+  };
+
+  //   이 방식이 리액트에서 공식적으로 권장하는 방식
+  //   const pageComp = () => {
+  //     let pageArray = [];
+
+  //     for (let i = 0; i < page; i++) {
+  //       pageArray.push(
+  //         <button
+  //           key={i}
+  //           className={`ml-4 text-2xl font-bold hover:text-white ${
+  //             i + 1 === selectedPage ? "text-white" : "text-gray-400"
+  //           }`}
+  //           onClick={() => onClickPageChange(i)}
+  //         >
+  //           {i + 1} <span className="text-base">페이지</span>
+  //         </button>
+  //       );
+  //     }
+  //     return pageArray;
+  //   };
+
+  //   const onClickPageChange = (p) => {
+  //     setSelectedPage(p);
+  //   };
+  //   useEffect(() => {
+  //     // console.log(page);
+  //     getNfts(page);
+  //   }, [page]);
+
   useEffect(() => {
-    getNfts();
-  }, []);
+    console.log(nfts);
+    getNfts(page);
+  }, [page]);
 
   return (
     <>
       <Box
-        ref={ref}
+        // ref={ref}
         w={"100%"}
         h={"600px"}
         bgGradient={"linear(to-l,#8c1eaa,#272842)"}
@@ -115,23 +207,53 @@ const Calender = forwardRef((props, ref) => {
                   // style={{ "background-color": "black" }}
                 />
               </CalendarContainer>
-              <div>
-                {nfts &&
-                  nfts.map((image, index) => (
-                    <img key={index} src={image.url} alt={image.title} />
-                  ))}
+              <div className="max-w-screen-xl mx-auto pt-4">
+                <div>{pageComp()}</div>
+                <ul className="mt-8 grid grid-cols-1 xl:grid-cols-2 justify-items-center gap-8">
+                  {nfts ? (
+                    nfts.map((v, i) => {
+                      return (
+                        <div className="rounded-2xl bg-gray-800 pb-4 relative">
+                          {parseInt(mintedNft) < v.tokenId && (
+                            <div className="absolute bg-black w-full h-full bg-opacity-50 rounded-2xl flex justify-center items-center text-4xl font-bold pb-[200px]">
+                              not Minted
+                            </div>
+                          )}
+                          <img
+                            className="rounded-t-2xl"
+                            src={v.metadata.image}
+                            alt={v.metadata.name}
+                          />
+                          <div className="mt-4 text-xl font-bold flex items-center px-4 text-gray-300">
+                            Da Den Bu
+                            {/* <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center ml-2 text-gray-950">
+                              <FaChessRook size={16} />
+                            </div> */}
+                          </div>
+                          <div className="mt-4"># {v.tokenId}</div>
+                          <div className="mt-4 text-xl flex justify-end px-4">
+                            <Link to={`/${v.tokenId}`}>
+                              <button
+                                disabled={parseInt(mintedNft) < v.tokenId}
+                                className="bg-gray-50 text-gray-950 px-4 py-2 rounded-xl hover:bg-gray-500"
+                              >
+                                Detail
+                              </button>
+                            </Link>
+                          </div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div>로딩중입니다...</div>
+                  )}
+                </ul>
               </div>
               <div className="text-gray-500 mt-4">{formattedDate}</div>
               {/* <MyCalendar /> */}
             </div>
           </Box>
-          <Box
-            borderRadius={"30px"}
-            h="100%"
-            bg="white"
-            ml={"20px"}
-            flex="1"
-          ></Box>
+
           {/* 큰 캘린더 */}
           {/* <Box borderRadius={"30px"} h="100%" bg="gray.500" mr={"20px"} flex="2"></Box> */}
 
@@ -274,141 +396,10 @@ const Calender = forwardRef((props, ref) => {
           </Box>
 
           {/* 작은 캘린더 민팅 이전 */}
-          <Box
-            direction={"column"}
-            justify={"center"}
-            align={"center"}
-            borderRadius={"30px"}
-            h="100%"
-            bg="white"
-            ml={"20px"}
-            flex="1"
-          >
-            <Box>
-              {/* #1 top box */}
-              <Flex mt={"20px"} justify={"space-around"} align={"center"}>
-                <Image src={previousMonth} />
-                <Flex
-                  fontFamily="Poppins"
-                  fontStyle="normal"
-                  fontWeight={700}
-                  fontSize={["20px", null, "25.7447px"]}
-                  lineHeight={["23px", null, "39px"]}
-                  color="#000000"
-                >
-                  Date's
-                </Flex>
-                <Image src={nextMonth} />
-              </Flex>
-              {/* #2 middle box */}
-              <Box
-                background="rgba(0, 0, 0, 0)"
-                position="relative"
-                boxSizing="border-box"
-                left="0px"
-                top="50px"
-                border="1px solid #ADADAD"
-                borderRadius="30px"
-                w={"300px"}
-                h={"350px"}
-              ></Box>
-              {/* #3 below box */}
-              <Box
-                background="linear-gradient(180deg, rgba(52, 71, 88, 0.9) 0%, rgba(56, 89, 120, 0) 100%)"
-                position="relative"
-                boxSizing="border-box"
-                left="0px"
-                top="72px"
-                border="1px solid #ADADAD"
-                borderTopLeftRadius="0px"
-                borderTopRightRadius="0px"
-                borderBottomRightRadius="30px"
-                borderBottomLeftRadius="30px"
-                w={"100%"}
-                h={"80px"}
-              >
-                <Flex
-                  direction={"column"}
-                  justify={"flex-start"}
-                  align={"flex-start"}
-                >
-                  <Box
-                    mt={"10px"}
-                    ml={"20px"}
-                    fontFamily={"Raleway"}
-                    fontStyle={"normal"}
-                    fontWeight={"700"}
-                    fontSize={"16px"}
-                    lineHeight={"23px"}
-                    color={"#FFFFFF"}
-                    justify={"center"}
-                    align={"center"}
-                  >
-                    Date
-                  </Box>
-
-                  <Flex position={"relative"}>
-                    <Image
-                      src={DescriptionStarLight2}
-                      position={"absolute"}
-                      left={"-20px"}
-                      bottom={"-30px"}
-                      width={"100px"}
-                      height={"100px"}
-                    />
-                    <Text
-                      ml={"50px"}
-                      mb={"2px"}
-                      fontFamily={"Raleway"}
-                      fontStyle={"normal"}
-                      fontWeight={"700"}
-                      fontSize={"20px"}
-                      lineHeight={"38px"}
-                      color={"#FFFFFF"}
-                      // position={"relative"}
-                    >
-                      20230522
-                    </Text>
-
-                    <Button
-                      mt={"5px"}
-                      ml={"120px"}
-                      w={"120px"}
-                      h={"30px"}
-                      borderRadius={"20px"}
-                      _hover={{ bg: "purple.500" }}
-                    >
-                      <Popover trigger={"hover"} placement={"bottom-start"}>
-                        <PopoverTrigger>
-                          <Box
-                            fontWeight={"500"}
-                            color="black"
-                            fontStyle={"normal"}
-                            fontSize={{ base: "10px", md: "15px" }}
-                            lineHeight={"30px"}
-                            justifyContent={"flex-end"}
-                            alignItems={"flex-end"}
-                            _hover={{
-                              textDecoration: "none",
-                              color: "linkHoverColor",
-                            }}
-                            href="/"
-                            target="_blank"
-                          >
-                            Mint the date
-                          </Box>
-                        </PopoverTrigger>
-                      </Popover>
-                    </Button>
-                  </Flex>
-                </Flex>
-              </Box>
-            </Box>
-          </Box>
         </Flex>
       </Box>
     </>
   );
-});
+};
 
 export default Calender;
