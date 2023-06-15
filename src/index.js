9977e74b26c35033317162ad42802a84a4b6ba11
likejjw <jjw3310@gmail.com>
2023-06-16 05:14:22 +0900

    set coreW

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { Web3ConnectionContextProvider } from "./components/context/web3Connection.context";
+import { Web3ConnectionContextProvider } from "./components/context/web3Connection.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //   <Web3ConnectionContextProvider>
-  <App />
-  //   </Web3ConnectionContextProvider>
+  <Web3ConnectionContextProvider>
+    <App />
+  </Web3ConnectionContextProvider>
 );
